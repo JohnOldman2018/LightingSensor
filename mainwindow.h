39d187bbdd46d10ab5bc2032950a3ce84834e298
JohnOldman2018 <36810779+JohnOldman2018@users.noreply.github.com>
2018-12-19 17:09:03 +0200

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,15 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_lineEdit_returnPressed();
+
+    void on_pushButton_pressed();
+
 private:
     Ui::MainWindow *ui;
+    void writeToFile(QString writeLine, QString fileName);
+    void readFromFile(QString fileName);
 };
 
 #endif // MAINWINDOW_H
